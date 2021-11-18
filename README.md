6b8cb87ab8b20b50e24d08d6995aa5709f7315e7
Kaven <kaven@wuwenkai.com>
2021-11-18 18:08:10 +0800

    Use 80 port by default

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # kaven-file-server
+
+## Docker
+
+```sh
+docker run -d -it --name kaven-file-server \
+    -p 3000:80 \
+    -v "$(pwd)"/uploads:/app/uploads \
+    kavenzero/kaven-file-server:main
+```
--- index.js
+++ index.js
@@ -4,9 +4,9 @@
  * @website:     http://blog.kaven.xyz
  * @file:        [kaven-file-server] /index.js
  * @create:      2021-11-18 15:55:12.122
- * @modify:      2021-11-18 16:32:12.690
+ * @modify:      2021-11-18 18:05:24.938
  * @version:     1.0.1
- * @times:       6
+ * @times:       7
  * @lines:       27
  * @copyright:   Copyright © 2021 Kaven. All Rights Reserved.
  * @description: [description]
@@ -16,11 +16,11 @@
 const { join } = require("path");
 const express = require("express");
 
-const port = 3000;
+const port = 80;
 const app = express();
 
 app.use("/", require("./server")(join(__dirname, "uploads")));
 
-app.listen(3000, "0.0.0.0", () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
