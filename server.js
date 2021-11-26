658d713ee4aeb8eddd8214c420fd8fb8264a89b9
Kaven <kaven@wuwenkai.com>
2021-11-26 12:29:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@
  * @website:     http://blog.kaven.xyz
  * @file:        [kaven-file-server] /server.js
  * @create:      2021-11-18 15:22:36.251
- * @modify:      2021-11-26 12:27:38.346
+ * @modify:      2021-11-26 12:28:54.079
  * @version:     1.0.3
- * @times:       32
+ * @times:       33
  * @lines:       145
  * @copyright:   Copyright © 2021 Kaven. All Rights Reserved.
  * @description: [description]
@@ -18,7 +18,7 @@ const fs = require("fs");
 const multer = require("multer");
 const { FileSize, IsString, Distinct } = require("kaven-utils");
 const { join, isAbsolute } = require("path");
- 
+
 /**
   * 
   * @param {string} uploadRootDir 
@@ -28,37 +28,37 @@ const { join, isAbsolute } = require("path");
   * @returns 
   */
 module.exports = function KavenFileServer(uploadRootDir, filedFile = "file", fieldDir = "dir", allowUploadToSubDir = true) {
- 
+
     if (!uploadRootDir) {
         throw new Error("dir is required.");
     }
- 
+
     const router = Router();
- 
+
     const tryGetField = (from, name) => {
         if (!from || !name) {
             return undefined;
         }
- 
+
         let result = from[name];
         if (result === undefined) {
             return result;
         }
- 
+
         if (IsString(result)) {
             return result;
         }
- 
+
         if (Array.isArray(result)) {
             result = Distinct(result);
             if (result.length === 1) {
                 return result[0];
             }
         }
- 
+
         return undefined;
     };
- 
+
     // https://github.com/expressjs/multer/issues/914#issuecomment-654084057
     // Note that req.body might not have been fully populated yet.
     // It depends on the order that the client transmits fields and files to the server.
@@ -67,7 +67,7 @@ module.exports = function KavenFileServer(uploadRootDir, filedFile = "file", fie
         destination: function(req, file, cb) {
             try {
                 let saveDir = uploadRootDir;
- 
+
                 if (allowUploadToSubDir) {
                     const fieldName = `${file.fieldname}_dir`;
                     const subDir = tryGetField(req.body, fieldName) || tryGetField(req.body, fieldDir);
@@ -75,12 +75,12 @@ module.exports = function KavenFileServer(uploadRootDir, filedFile = "file", fie
                         saveDir = join(uploadRootDir, subDir);
                     }
                 }
-             
+
                 if (!fs.existsSync(saveDir)) {
                     fs.mkdirSync(saveDir, { recursive: true });
                     console.log(`mkdir: ${saveDir}`);
                 }
-             
+
                 cb(null, saveDir);
             } catch (ex) {
                 cb(ex);
@@ -89,48 +89,48 @@ module.exports = function KavenFileServer(uploadRootDir, filedFile = "file", fie
         filename: function(req, file, cb) {
             try {
                 let saveName = file.originalname;
- 
+
                 const fieldName = `${file.fieldname}_name`;
                 const name = tryGetField(req.body, fieldName);
                 if (name) {
                     saveName = name;
                 }
-                 
+
                 cb(null, saveName);
             } catch (ex) {
                 cb(ex);
             }
         },
     });
- 
+
     const m = multer({
         storage: storage,
     });
     const upload = filedFile ? m.array(filedFile) : m.any();
- 
+
     router.get("/", (req, res) => {
         res.send("Kaven File Server");
     });
- 
+
     router.post("/file", (req, res) => {
         upload(req, res, async function(err) {
             try {
                 if (err) {
                     console.error(err);
- 
+
                     // An error occurred when uploading
                     return res.status(400).send(err.message);
                 }
- 
+
                 for (const file of req.files) {
                     let log = `file uploaded: ${file.path}, ${FileSize(file.size)}`;
                     if (file.originalname !== file.filename) {
                         log += `, originalname: ${file.originalname}`;
                     }
-                     
+
                     console.log(log);
                 }
- 
+
                 // Everything went fine
                 return res.sendStatus(200);
             } catch (ex) {
@@ -139,7 +139,6 @@ module.exports = function KavenFileServer(uploadRootDir, filedFile = "file", fie
             }
         });
     });
- 
+
     return router;
 };
- 
